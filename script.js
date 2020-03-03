37f3bd8702b9d5dbb991204716507a11757d9a86
Nathan Long <nrlong13@gmail.com>
2020-03-03 11:55:55 -0500

    updated for uv index call.  tested functional

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
-// const key = "198e7cd123c38028748d31ffb347ffa7";
-// let URL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + key;
+$(document).ready(function () {
 
-$(".searchBtn").click(function(){
-    let search = $(this).prev().val()
-    const key = "198e7cd123c38028748d31ffb347ffa7";
+    $(".searchBtn").click(function () {
+        // $("#cityInfo").empty();
 
-    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + key;
+        let search = $(this).prev().val()
 
-    $.ajax({
-        url: URL,
-        method: "GET"
+        const key = "198e7cd123c38028748d31ffb347ffa7";
+
+        const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + key;
+
+        //
+        $.ajax({
+                url: URL,
+                method: "GET"
+            })
+            .then(function (response) {
+                let cityLoc = $("#city");
+                cityLoc.text(response.name);
+
+                let temp = $("#temperature");
+                let tempConvert = (((response.main.temp - 273.15) * (9 / 5)) + 32);
+
+                temp.text("Temperature: " + tempConvert.toFixed(1) + "°F");
+
+                let humidity = $("#humidity");
+                humidity.text("Humidity: " + response.main.humidity + "%");
+
+                let windSpeed = $("#windSpeed");
+                windSpeed.text("Wind Speed: " + response.wind.speed + "MPH");
+
+                //generate lon and lat for uv index pull
+                let lon = response.coord.lon;
+                let lat = response.coord.lat;
+
+                console.log(lon, lat);
+
+
+                // get information for UV index
+                const URLUV = "http://api.openweathermap.org/data/2.5/uvi?appid=" + key + "&lat=" + lat + "&lon=" + lon;
+
+                $.ajax({
+                        url: URLUV,
+                        method: "GET"
+                    })
+                    .then(function (responseUV) {
+                        let uvIndex = $("#uvIndex");
+                        uvIndex.text("UV Index: " + responseUV.value);
+
+                        console.log(responseUV);
+                    })
+
+
+
+
+
+                console.log(response);
+            });
+
+
+
     })
-    .then(function(response){
-        let cityLoc = $("#city");
-        cityLoc.text(response.name);
-
-        let temp = $("#temperature");
-        let tempConvert = (((response.main.temp - 273.15)  * (9/5)) + 32);
-
-        temp.text("Temperature: " + tempConvert.toFixed(1) + "°F");
-
-        let humidity = $("#humidity");
-        humidity.text("Humidity: " + response.main.humidity + "%");
-
-        let windSpeed = $("#windSpeed");
-        windSpeed.text("Wind Speed: " + response.wind.speed + "MPH");
-
-        // let uvIndex = 
-
-
-    
-        console.log(response);
-    });
 
 })
-
