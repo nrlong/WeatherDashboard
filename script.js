19df56e56e0dd9912d76678685ebc12ce4f747c8
Nathan Long <nrlong13@gmail.com>
2020-03-03 11:02:03 -0500

    updated output for current day weather.  tested functional.  search updated with side nav bar.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(".searchBtn").click(function(){
     let search = $(this).prev().val()
     const key = "198e7cd123c38028748d31ffb347ffa7";
 
-    const URL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + key;
+    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + key;
 
     $.ajax({
         url: URL,
@@ -13,11 +13,20 @@ $(".searchBtn").click(function(){
     })
     .then(function(response){
         let cityLoc = $("#city");
-        cityLoc.text(response.city.name);
-        let temp = $("#temperature");
-        let tempConvert = (((response.list[0].main.temp - 273.15)  * (9/5)) + 32);
+        cityLoc.text(response.name);
 
-        temp.text(tempConvert.toFixed(1) + "°F");
+        let temp = $("#temperature");
+        let tempConvert = (((response.main.temp - 273.15)  * (9/5)) + 32);
+
+        temp.text("Temperature: " + tempConvert.toFixed(1) + "°F");
+
+        let humidity = $("#humidity");
+        humidity.text("Humidity: " + response.main.humidity + "%");
+
+        let windSpeed = $("#windSpeed");
+        windSpeed.text("Wind Speed: " + response.wind.speed + "MPH");
+
+        // let uvIndex = 
 
 
     
