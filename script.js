3fe30719bc7a4d1efc7417187400ee61c461a7f4
Nathan Long <nrlong13@gmail.com>
2020-03-05 16:27:13 -0500

    updated for 5 day weather output with temp converstion and css output to inlineblock

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
     let historyArray = []
 
+    
+    // function to create history array on search
     function historyLink(){
         $("#searchHistory").empty();
         for (let i = 0; i < historyArray.length; i++){
@@ -15,10 +17,8 @@ $(document).ready(function () {
         }
         console.log(historyArray);
     }
-    
 
-
-   
+    // search button click and all the goodness that goes with it
 
     $(".searchBtn").click(function (event) {
         event.preventDefault();
@@ -26,15 +26,26 @@ $(document).ready(function () {
 
         const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + key;
 
-        //
+        // call for weather infomration on current forcast
         $.ajax({
                 url: URL,
                 method: "GET"
             })
+
+            // generate information to populate created elements for todays weather
             .then(function (response) {
                 let cityLoc = $("#city");
                 cityLoc.text(response.name);
 
+                let todayDate = moment.unix(response.dt).format("MMMM Do, YYYY");
+
+                let today = $("#today");
+                today.text(todayDate)
+                
+                console.log(todayDate);
+                
+
+                // convert from kelvin to °f
                 let temp = $("#temperature");
                 let tempConvert = (((response.main.temp - 273.15) * (9 / 5)) + 32);
 
@@ -88,14 +99,37 @@ $(document).ready(function () {
 
                 console.log(response);
             });
-
+        
         const URL5Day = "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + key;
 
+        // call for 5 day weather forcast
         $.ajax({
             url: URL5Day,
             method: "GET"
         })
         .then(function (response5Day) {
+            
+            for (let i = 0; i < 5; i++){
+            let timeChange = i * 8 + 7;
+            let fiveDayDate = moment.unix(response5Day.list[timeChange].dt).format("MMMM Do, YYYY");
+
+            let forcastDiv = $("#forecast");
+
+            let fiveDayDateDiv = $("<div>").attr("class", "five-day")
+            fiveDayDateDiv.text(fiveDayDate);
+
+            forcastDiv.append(fiveDayDateDiv);
+
+            
+            let forecast = $("<div>").attr("id", "temp-" + i);
+            forecast.text("Temperature: " + parseInt(((response5Day.list[timeChange].main.temp)- 273.15) * (9 / 5) + 32) +"°f");
+            
+            fiveDayDateDiv.append(forecast);
+
+        }
+
+            
+
             console.log(response5Day);
         })
 
@@ -107,8 +141,6 @@ $(document).ready(function () {
 
         historyLink();
     })
-
-    
 
     //allow enter keypress to search
 
