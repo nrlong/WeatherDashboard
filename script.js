41bbb6673335017270a50fffb8ef6c7428802f76
Nathan Long <nrlong13@gmail.com>
2020-03-07 08:06:00 -0500

    sepearted funcitons for search.  side history buttons now work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     const key = "198e7cd123c38028748d31ffb347ffa7";
-
+    
     let historyArray = []
 
     // function to create history array on search
@@ -10,23 +10,36 @@ $(document).ready(function () {
         for (let i = 0; i < historyArray.length; i++) {
             let newLink = $("<button>").attr("class", "historyBtn");
             newLink.addClass("history");
-            newLink.attr("data-name", historyArray[i]);
+            newLink.attr("id", historyArray[i]);
             newLink.text(historyArray[i]);
             $("#searchHistory").append(newLink);
         }
-        console.log(historyArray);
     }
 
     // search button click and all the goodness that goes with it
-    $(".searchBtn").click(function weatherSearch (event) {
+    $(".searchBtn").click(function (event) {
         event.preventDefault();
-
+        search = $("#search").val().trim();
         // empty generated elements with each search to avoid duplication
         $("#weatherIcon").empty();
         $("#forecast").empty();
+        weatherSearch();
+        historyLink();
+    });
 
-        let search = $(this).prev().val()
+    //allow history links to create search without creating new buttons
+    $(document).on("click", ".historyBtn", function(event){
+        event.preventDefault();
+        // empty generated elements with each search to avoid duplication
+        $("#weatherIcon").empty();
+        $("#forecast").empty();
+        search = $(this).attr("id")
+        weatherSearch();
 
+    });
+    
+    function weatherSearch(){
+        
         const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + key;
 
         // call for weather infomration on current forcast
@@ -45,15 +58,11 @@ $(document).ready(function () {
                 let today = $("#today");
                 today.text(todayDate)
 
-                console.log(todayDate);
-
                 let iconToday = $("#weatherIcon");
                 let getIcon = response.weather[0].icon;
 
-                console.log(getIcon);
                 let iconImage = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + getIcon + "@2x.png");
                 iconToday.append(iconImage);
-
 
                 // convert from kelvin to °f
                 let temp = $("#temperature");
@@ -71,10 +80,7 @@ $(document).ready(function () {
                 let lon = response.coord.lon;
                 let lat = response.coord.lat;
 
-                console.log(lon, lat);
-
-
-                // get information for UV index
+                //get information for UV index
                 const URLUV = "https://api.openweathermap.org/data/2.5/uvi?appid=" + key + "&lat=" + lat + "&lon=" + lon;
 
                 $.ajax({
@@ -102,17 +108,14 @@ $(document).ready(function () {
                             uvValue.attr("class", "extreme");
                         }
 
-                        console.log(typeof (responseUV.value));
-
                         uvIndex.append(uvValue);
                     })
 
-                console.log(response);
             });
 
         const URL5Day = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + key;
 
-        // call for 5 day weather forcast
+        //call for 5 day weather forcast
         $.ajax({
                 url: URL5Day,
                 method: "GET"
@@ -133,12 +136,10 @@ $(document).ready(function () {
                     let icon5Day = $("<div>").attr("class", "weatherIcon5Day");
                     let getIcon5Day = response5Day.list[timeChange].weather[0].icon;
 
-                    console.log(getIcon5Day);
                     let iconImage5Day = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + getIcon5Day + "@2x.png").attr("class", "five-day-icon");
                     icon5Day.append(iconImage5Day);
 
                     fiveDayDateDiv.append(icon5Day);
-
 
                     let forecast = $("<div>").attr("id", "temp-" + i);
                     forecast.text("Temperature: " + parseInt(((response5Day.list[timeChange].main.temp) - 273.15) * (9 / 5) + 32) + "°f");
@@ -152,20 +153,19 @@ $(document).ready(function () {
                     fiveDayDateDiv.append(fiveDayHumidOut);
                 }
 
-                console.log(response5Day);
             })
 
         let searchText = $("#search").val().trim();
 
-        console.log(searchText);
         historyArray.push(searchText);
 
-        historyLink();
         
+    }
+    
+
     })
 
     //allow enter keypress to search
-
 
     $("#searchDiv").keypress(function (event) {
 
@@ -175,4 +175,4 @@ $(document).ready(function () {
         }
     });
 
-});
+;
